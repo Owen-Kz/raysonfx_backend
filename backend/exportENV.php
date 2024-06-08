e2a024b561b8e530704eb8da5667f2b4729cdb75
Benson Michael Owen <lordkz19@gmail.com>
2024-06-08 12:22:09 +0100

    dotENV

--- backend/exportENV.php
+++ backend/exportENV.php
@@ -2,7 +2,10 @@
 
 require_once __DIR__ .'/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
 
+// Load environment variables from .env in local development
+if (file_exists(__DIR__ . '/../.env')) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+    $dotenv->load();
+}
 
