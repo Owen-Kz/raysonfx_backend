21f10385d325f438db2c4b37cdc118eefdf39a00
Benson Michael Owen <lordkz19@gmail.com>
2024-06-08 03:14:01 +0100

    Doman changed

--- backend/account/rejectWithdrawal.php
+++ backend/account/rejectWithdrawal.php
@@ -35,7 +35,7 @@ if(isset($userID)){
 
             $response = array("status" => "succsss", "message" => "Transaction Declined Successfully");
             echo json_encode($response);
-            header('Location: https://raysonfinance.vercel.app/foreman/dashboard');
+            header('Location: https://www.raysonfinance.org/foreman/dashboard');
            }else{
             $response = array("status" => "error", "message" =>  "Error: " . $stmt->error);
            echo json_encode($response);
