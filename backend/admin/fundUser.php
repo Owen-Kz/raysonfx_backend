d9404061d870fde530f2a388bee1d6e9037f8144
Benson Michael Owen <lordkz19@gmail.com>
2024-08-06 14:22:20 +0100

    Fund CORS

--- backend/admin/fundUser.php
+++ backend/admin/fundUser.php
@@ -1,7 +1,23 @@
 <?php
-include __DIR__."../cors.php";
+// include __DIR__."../cors.php";
 // enableCORS(); 
+if (isset($_SERVER['HTTP_ORIGIN'])) {
+    // Allow from any origin
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+    header('Access-Control-Allow-Credentials: true');
+    header('Access-Control-Max-Age: 86400');    // cache for 1 day
+}
 
+// Access-Control headers are received during OPTIONS requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    }
+    if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+    }
+    exit(0);
+}
 include '../db.php';
 session_start();
 include "../sendTransactionEmail.php"; 
